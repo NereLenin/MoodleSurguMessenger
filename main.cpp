5abbce138c441f66d8f27dbd35ab79debd5b51b8
NereLenin <NereLenin25@yandex.ru>
2022-06-18 23:32:54 +0500

    add all stuffT

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QSslSocket>
+#include "appengine.h"
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+//MoodleNetworkManager/MoodleNetworkManager.qml
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qml/MoodleSurguMessenger.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+
+    engine.load(url);
+
+    QString result = "false";
+
+    if(QSslSocket::supportsSsl()) result = "true";
+    qDebug() << "Device supports OpenSSL: " << QSslSocket::supportsSsl();
+
+    engine.rootContext()->setContextProperty("InetResult",result);
+
+    MyAppEngine appEngine(engine.rootContext(),&engine);
+    appEngine.checkConnection();
+
+    return app.exec();
+}
---
+++ src/network/moodlenetworkmanager.h
@@ -0,0 +1,77 @@
+#ifndef MOODLENETWORKMANAGER_H
+#define MOODLENETWORKMANAGER_H
+
+#include <QObject>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkCookieJar>
+#include <QtNetwork/QNetworkReply>
+
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFile>
+#include <QJsonParseError>
+
+#include <QTimer>
+#include <QEventLoop>
+#include <QRegularExpression>
+
+
+#include <src/conversation/conversation.h>
+#include <src/profiles/account.h>
+#include <iostream>
+
+using namespace std;
+
+//pars
+bool ParsString(QString RegExp,QByteArray StringForPars,QString& result,int from=0);
+
+class MoodleNetworkManager: protected QObject
+{
+    Q_OBJECT
+private:
+
+    //network
+    QNetworkAccessManager manager;// rename network manager
+    QNetworkCookieJar cookieJar;
+    QNetworkRequest request;
+    QUrl CurrentUrl;
+
+
+    QTimer ResponseTimer;
+
+    QEventLoop ResponseLoop;
+
+    bool isGoodAuthorisation;
+
+    bool CreateGetWaitiedResponse(QString url, QByteArray *GetReply=nullptr);
+    bool CreatePostWaitiedResponse(QString url,QString data, QByteArray *PostReply=nullptr);
+    bool CreatePostWaitiedResponse(QString url, QByteArray data,QByteArray *PostReply);
+
+    void ReadJsonConversation(QJsonValue conv,Conversation &RewritedConv);
+    bool ReadJson(const QByteArray &pars,QList<Conversation*> &ConversationToRead);
+    bool ReadJson(const QByteArray &pars,Conversation& WriteTo);
+
+public:
+    explicit MoodleNetworkManager(QObject *parent=nullptr);
+    Account current_account;
+
+    void SetLogin(const QString& Login);
+    void SetPassword(const QString& Password);
+
+    bool isNetworkAccessible();
+    bool isLogIn();
+
+    void Authorisation();
+
+    void ReadExistConversations(QList<Conversation*> &ConversationToRead);
+    void ReadConversation(Conversation& WriteTo);
+
+    void SendMessage(Conversation& Addressee, QString Text);
+    void DeleteMessage(Message& DeletedMessage);
+    void DeleteMessage(int MessageId);
+
+    void LogOut();
+};
+
+#endif // MOODLENETWORKMANAGER_H
